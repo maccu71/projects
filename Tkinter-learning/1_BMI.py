b08447a4ea13495ee079952d4b4bfb5dc9522ae1
maccu@poczta.onet.pl <maccu@poczta.onet.pl>
2025-02-15 16:00:15 +0100

    added OOP do 1_bmi.py

--- Tkinter-learning/1_BMI.py
+++ Tkinter-learning/1_BMI.py
@@ -1,33 +1,58 @@
-def oblicz_bmi(wzrost: int, waga: int) -> float:
-    """
-    Prosty program napisany w języku Python3
-    do obliczania BMI (współczynnika BMI - Body Mass Index)
-    wskazującego stosunek masy ciała do jego wzrostu.
-    Dodatkowe tabele pozwalaja ocenić jego stan.
-    Parametrami są tutaj:
-    waga - masa ciała w kilogramach
-    wzrost - wzrost w centymetrach
-    Zwracanym wynikiem jest:
-    bmi - współczynnik BMI z dokładnością do jednej liczby po przecinku
-    """
-    bmi = waga / wzrost   # silnik obliczanie BMI
-    return bmi
+import tkinter as tk
 
-if __name__ == "__main__":
-    while True:
+class App:
+    def __init__(self, window):
+        '''
+        program do obliczania BMI wykorzystujący
+        tkinter framework
+        '''
+        self.window = window
+        self.window.title("BMI kalkulator")
+
+        self.label1 = tk.Label(self.window, text='Podaj wagę w kg')
+        self.label1.pack()
+
+        self.entry1 = tk.Entry(self.window)
+        self.entry1.pack()
+
+        self.label2 = tk.Label(self.window, text='Podaj wzrost w cm')
+        self.label2.pack()
+
+        self.entry2 = tk.Entry(self.window)
+        self.entry2.pack()
+
+        self.wynik = tk.Label(self.window, text="Your BMI will appear here.")
+        self.wynik.pack()
+
+        self.close = tk.Button(self.window, text='Tutaj zamknij aplikację', command=self.close_app)
+        self.close.pack()
+
+        self.oblicz = tk.Button(self.window,text='Oblicz swoje BMI',command=self.oblicz_bmi)
+        self.oblicz.pack()
+
+    def close_app(self) -> None:
+        '''
+        ma za zadanie zamknąć aplikację 
+        po naciśnięciu przycisku'''
+        self.window.destroy()
+
+    def oblicz_bmi(self) -> None:
+        '''
+        ma na celu obliczenie BMI - główne obliczenia
+        '''
         try:
-            waga = input("wprowadź swoją wagę w kg: ")
-            waga = int(waga)
-            wzrost = input("wprowadź swoją wagę w cm: ")
-            wzrost = int(wzrost)
-            break
-        except ValueError:
-            print("wprowadź jeszcze raz poprawne wartości..")
-            continue
-    obliczony_bmi = oblicz_bmi(waga, wzrost)
-    print(f'Twój index BMI wynosi: {obliczony_bmi:.1f}')
+            waga = float(self.entry1.get())
+            wzrost = float(self.entry2.get())/100
+            print(waga, wzrost)
+            wynik = waga / (wzrost * wzrost) 
+            print(f'{wynik:.2f}')
+            self.wynik.config(text=f'Twoje BMI: {wynik:.2f}')
+        except:
+            self.wynik.config(text='Podaj prawidłowe wartości w cm i kg')
 
+window = tk.Tk()
+app = App(window)
 
-
+window.mainloop()
 
 
